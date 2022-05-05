4390347936e989ffcdea43fcb59655283502f302
AngelNavarroTirado <angel_navarrotirado@iescarlesvallbona.cat>
2022-05-05 16:41:25 +0200

    usAdapter

--- app/src/main/java/com/example/mymovielist/models/Users/usAdapter.kt
+++ app/src/main/java/com/example/mymovielist/models/Users/usAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.mymovielist.models.Users
 
-class usAdapter() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mymovielist.R
+import com.example.mymovielist.databinding.UserItemBinding
+
+class usAdapter(
+    private val user: User)
+    : RecyclerView.Adapter<usAdapter.usViewHolder>() {
+    class usViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = UserItemBinding.bind(view)
+
+        fun binUser(image:String?,name : String?){
+            Glide.with(itemView).load(image).into(binding.ivUserItemImatge)
+            binding.tvUserItemName.text = name
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): usViewHolder {
+        return usViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: usViewHolder, position: Int) {
+        val usuari = user
+        holder.binUser(usuari.image, usuari.name)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
