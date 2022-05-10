72b490c267d166a01735f376c780801b5d1fb78f
AngelNavarroTirado <angel_navarrotirado@iescarlesvallbona.cat>
2022-05-10 19:23:40 +0200

    Review al 90%
    adaptador + activity + item

--- app/src/main/java/com/example/mymovielist/Reviews_a.kt
+++ app/src/main/java/com/example/mymovielist/Reviews_a.kt
@@ -4,11 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mymovielist.databinding.ActivityReviewsBinding
+import com.example.mymovielist.models.ApiService
+import com.example.mymovielist.models.Review.ReviewUserAdapter
+import com.example.mymovielist.models.Review.Reviews
+import com.example.mymovielist.models.Users.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Reviews_a : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReviewsBinding
+    private var reviewsUser = mutableListOf<Reviews>()
+    private lateinit var adapter: ReviewUserAdapter
+
+    //private val shared : SharedPreferences = applicationContext.getSharedPreferences("Login", Context.MODE_PRIVATE)
+    //private val email = shared.getString("email", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reviews)
+        binding = ActivityReviewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listaReviewsUser()
 
         // Finestra pelicules
         val films1=findViewById<ImageButton>(R.id.bu_review_films)
@@ -40,10 +62,43 @@ class Reviews_a : AppCompatActivity() {
         // Finestra ranking
         val rank1=findViewById<ImageButton>(R.id.bu_review_rank)
         rank1.setOnClickListener {
-            val intento1 = Intent(this, FilmsYouSee::class.java)
+            val intento1 = Intent(this, Ranking::class.java)
             startActivity(intento1)
             overridePendingTransition(R.anim.animation0, R.anim.animation0)
             finish();
         }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://6o5zl5.deta.dev/users/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    private fun listaReviewsUser() {
+            CoroutineScope(Dispatchers.IO).launch {
+                val call = getRetrofit().create(ApiService::class.java)
+                    .getUser("rockyrockyUPDATED@example.com")
+
+                val user = call.body()
+
+                runOnUiThread {
+                    if (user != null) {
+                        initReviewsUser(user)
+
+                    }
+                }
+
+            }
+
+    }
+
+    private fun initReviewsUser(user: User){
+        for (u in user!!.reviews){
+            reviewsUser.add(u)
+            println(u)
+        }
+        adapter = ReviewUserAdapter(reviewsUser)
+        binding.rvReviewsUser.layoutManager = LinearLayoutManager(this)
+        binding.rvReviewsUser.adapter = adapter
     }
 }
--- app/src/main/java/com/example/mymovielist/models/Review/ReviewUserAdapter.kt
+++ app/src/main/java/com/example/mymovielist/models/Review/ReviewUserAdapter.kt
@@ -1,15 +1,44 @@
 package com.example.mymovielist.models.Review
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mymovielist.R
+import com.example.mymovielist.databinding.ReviewuserItemBinding
 
 class ReviewUserAdapter(
     private val reviews: List<Reviews>
     ) : RecyclerView.Adapter<ReviewUserAdapter.ReviewUserViewHolder>() {
 
         class ReviewUserViewHolder(view: View) : RecyclerView.ViewHolder(view){
-            private val binding =
+            private val binding = ReviewuserItemBinding.bind(view)
+            fun binReviewUser(imatge:String?, title: String?, point: Float?, coment: String?){
+                Glide.with(itemView).load("https://image.tmdb.org/t/p/w500"+imatge).into(binding.ivImgFilmIRu)
+                binding.tvTitolFilmIRu.text = title
+                binding.tvComentFilmIRu.text = coment
+                if (point != null) {
+                    binding.rbPointsFilmIRu.rating = (point.div(2)).toFloat()
+                }
+
+            }
         }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewUserViewHolder {
+        return ReviewUserViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.reviewuser_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ReviewUserViewHolder, position: Int) {
+        val result = reviews[position]
+        holder.binReviewUser(result.backdropPath, result.movieTitle, result.score, result.comment)
+    }
+
+    override fun getItemCount(): Int {
+        return reviews.size
+    }
 
 
 }
