e5f6abb6bc13557c551204bfba8c8554f0693ada
JordiSerraEmilio IES Carles Vallbona <jordi_serraemilio@iescarlesvallbona.cat>
2022-05-12 21:04:11 +0200

    Commit and Push de ---> Haciendo el RecommendedFilms, pero no funciona...

--- app/src/main/java/com/example/mymovielist/models/Recomended/Recomendedfilms.kt
+++ app/src/main/java/com/example/mymovielist/models/Recomended/Recomendedfilms.kt
@@ -1,6 +1,8 @@
 package com.example.mymovielist.models.Recomended
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -12,6 +14,7 @@ import com.example.mymovielist.models.ApiService
 import com.example.mymovielist.models.TopFilms.ResultsTop
 import com.example.mymovielist.models.TopFilms.TopAdapter
 import com.example.mymovielist.models.TopFilms.TopFilms
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,11 +22,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Recomendedfilms : AppCompatActivity() {
-    private lateinit var binding : ActivityRecomendedfilmsBinding
+    private lateinit var binding: ActivityRecomendedfilmsBinding
+
     private lateinit var adapter: TopAdapter
+
     private var pags = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     private var pag = 1
+
     private var films = mutableListOf<ResultsTop>()
+    private var recomendedFilms = mutableListOf<ResultsTop>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecomendedfilmsBinding.inflate(layoutInflater)
@@ -33,11 +41,13 @@ class Recomendedfilms : AppCompatActivity() {
 //            pag = x
 //            listaPeliculas(true)
 //        }
-        listaPeliculas(true)
+
+
+        listaPeliculas()
         println(films.toString())
 
         // Finestra Films you see
-        val yousee1=findViewById<ImageButton>(R.id.bu_films_yousee)
+        val yousee1 = findViewById<ImageButton>(R.id.bu_films_yousee)
         yousee1.setOnClickListener {
             val intento1 = Intent(this, FilmsYouSee::class.java)
             startActivity(intento1)
@@ -46,7 +56,7 @@ class Recomendedfilms : AppCompatActivity() {
         }
 
         // Finestra reviews
-        val review1=findViewById<ImageButton>(R.id.bu_films_review)
+        val review1 = findViewById<ImageButton>(R.id.bu_films_review)
         review1.setOnClickListener {
             val intento1 = Intent(this, Reviews_a::class.java)
             startActivity(intento1)
@@ -54,7 +64,7 @@ class Recomendedfilms : AppCompatActivity() {
             finish();
         }
         // Finestra users
-        val users1=findViewById<ImageButton>(R.id.bu_films_users)
+        val users1 = findViewById<ImageButton>(R.id.bu_films_users)
         users1.setOnClickListener {
             val intento1 = Intent(this, Users::class.java)
             startActivity(intento1)
@@ -63,7 +73,7 @@ class Recomendedfilms : AppCompatActivity() {
         }
 
         // Finestra ranking
-        val rank1=findViewById<ImageButton>(R.id.bu_films_rank)
+        val rank1 = findViewById<ImageButton>(R.id.bu_films_rank)
         rank1.setOnClickListener {
             val intento1 = Intent(this, Ranking::class.java)
             startActivity(intento1)
@@ -77,36 +87,43 @@ class Recomendedfilms : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create()).build()
     }
 
-    private fun listaPeliculas(boolean: Boolean) {
-        if(boolean == true){
-            CoroutineScope(Dispatchers.IO).launch {
-                val call = getRetrofit().create(ApiService::class.java)
-                    .getPopularFilms("top_rated?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US&page="+pag.toString())
+    private fun listaPeliculas() {
 
-                val peli = call.body()
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java)
+                .getPopularFilms("top_rated?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US&page=" + pag.toString())
 
-                runOnUiThread {
-                    if (peli != null) {
-                        initFilms(peli)
-                    }
-                }
+            val peli = call.body()
+            runOnUiThread {
+                initFilms(peli!!)
             }
-        }else{
-            delFilms()
+            val handler = CoroutineExceptionHandler { _, exception ->
+                println("CoroutineExceptionHandler got $exception")
+            }
         }
-
     }
 
-    private fun initFilms(topFilms: TopFilms){
-        for (film in topFilms!!.results){
+    private fun initFilms(topFilms: TopFilms) {
+        val shared: SharedPreferences =
+            applicationContext.getSharedPreferences("Login", Context.MODE_PRIVATE)
+        val email = shared.getString("email", "")
+
+        for (film in topFilms!!.results) {
             films.add(film)
+
+            //Filtro por generos
+//            for (movie in films) {
+//                if (movie.genreIds.contains(user.genres.id)) {
+//                    recomendedFilms.add(movie)
+//                }
+//            }
         }
         adapter = TopAdapter(films)
         binding.rvFilms.layoutManager = LinearLayoutManager(this)
         binding.rvFilms.adapter = adapter
     }
 
-    private fun delFilms(){
+    private fun delFilms() {
         films.removeAll { true }
     }
 
