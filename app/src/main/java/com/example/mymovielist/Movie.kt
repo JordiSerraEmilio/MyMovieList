a3ab3502a151e3d262b80507bc38be7ee5ff41be
AngelNavarroTirado <angel_navarrotirado@iescarlesvallbona.cat>
2022-05-17 20:37:50 +0200

    Actores hecho

--- app/src/main/java/com/example/mymovielist/Movie.kt
+++ app/src/main/java/com/example/mymovielist/Movie.kt
@@ -1,14 +1,19 @@
 package com.example.mymovielist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.compose.ui.graphics.Color
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.example.mymovielist.models.Actors.AdapterActor
+import com.example.mymovielist.models.Actors.Cast
 import com.example.mymovielist.models.TopFilms.PageAdapter
 import com.example.mymovielist.models.ApiService
 import com.example.mymovielist.models.Movies.AdapterCompanies
@@ -22,23 +27,57 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Movie : AppCompatActivity() {
-    var tabTitle = arrayOf("COMPANIES", "ACTORS","REVIEWS")
+
+    //var tabTitle = arrayOf("COMPANIES", "ACTORS","REVIEWS")
+    var i = 1;
+
     private var companies = mutableListOf<ProductionCompanies>()
+    private var actores = mutableListOf<Cast>()
+
     private lateinit var adapter: AdapterCompanies
+    private lateinit var adapter2: AdapterActor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
         val idmovie = intent.extras?.getInt("rid")
 
         bucarPorId(idmovie.toString())
+        CanviarRecycleView(idmovie.toString())
 
-        var pager = findViewById<ViewPager2>(R.id.viewPagerTopFilms)
-        var tab1 = findViewById<TabLayout>(R.id.tabs)
-        pager.adapter = PageAdapter(supportFragmentManager, lifecycle)
-        TabLayoutMediator(tab1, pager){
-                tab, position ->
-            tab.text = tabTitle[position]
-        }.attach()
+        val rank1=findViewById<TextView>(R.id.tv_bu_actors)
+            rank1.setOnClickListener {
+                if(i != 2){
+                if(i == 1){
+                    companies.removeAll {true}
+                }else if(i == 3){
+                }
+                i = 2
+                CanviarRecycleView(idmovie.toString())
+                }
+            }
+
+
+            val rank2 = findViewById<TextView>(R.id.tv_bu_producers)
+            rank2.setOnClickListener {
+                if(i != 1) {
+                if (i == 2) {
+                    actores.removeAll { true }
+                } else if (i == 3) {
+                }
+                i = 1
+                CanviarRecycleView(idmovie.toString())
+                }
+            }
+
+
+        //var pager = findViewById<ViewPager2>(R.id.viewPagerTopFilms)
+        //var tab1 = findViewById<TabLayout>(R.id.tabs)
+        //pager.adapter = PageAdapter(supportFragmentManager, lifecycle)
+        //TabLayoutMediator(tab1, pager){
+        //        tab, position ->
+        //    tab.text = tabTitle[position]
+        //}.attach()
 
         //setContent()
     }
@@ -67,7 +106,10 @@ class Movie : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val call = getRetrofit().create(ApiService::class.java)
                 .getMevie(id+"?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US")
+            val call2 = getRetrofit().create(ApiService::class.java)
+                .getActors(id+"/credits?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US")
             val body = call.body()
+            val body2 = call2.body()
             if (body != null) {
                 runOnUiThread {
                     var imatge = body.backdropPath
@@ -128,11 +170,90 @@ class Movie : AppCompatActivity() {
                             }
                         }
                     }
-                    if(body.productionCompanies != null){
-                        initCompanies(body.productionCompanies)
+
+                }
+            }
+
+
+        }
+    }
+
+    private fun CanviarRecycleView(id: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java)
+                .getMevie(id+"?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US")
+            val call2 = getRetrofit().create(ApiService::class.java)
+                .getActors(id+"/credits?api_key=902a2e71fa0c8a74cbe2fc39a4560b99&language=en-US")
+            val body = call.body()
+            val body2 = call2.body()
+            if (i == 1){
+                if (body != null) {
+                    runOnUiThread {
+                        for (x in body.genres) {
+                            when (x.id) {
+                                "28" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Action"
+                                "12" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Adventure"
+                                "16" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Animation"
+                                "35" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Comedy"
+                                "80" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Crime"
+                                "99" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Documentary"
+                                "18" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Drama"
+                                "10751" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Family"
+                                "14" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Fantasy"
+                                "36" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " History"
+                                "27" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Horror"
+                                "10402" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Music"
+                                "9648" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Mystery"
+                                "10749" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Romance"
+                                "878" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Science Fiction"
+                                "10770" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " TV Movie"
+                                "53" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Thriller"
+                                "10752" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " War"
+                                "37" -> findViewById<TextView>(R.id.tv_film_genres).text =
+                                    findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Western"
+                                else -> { // Note the block
+                                    findViewById<TextView>(R.id.tv_film_genres).text =
+                                        findViewById<TextView>(R.id.tv_film_genres).text.toString() + " Undefined genre"
+                                }
+                            }
+                        }
+                        if(body.productionCompanies != null){
+                            initCompanies(body.productionCompanies)
+                        }
                     }
                 }
             }
+            else if (i == 2){
+                if (body2 != null){
+                    runOnUiThread {
+                        if (body2 != null) {
+                            println(body2.cast)
+                            initActors(body2.cast)
+                        }
+                    }
+                }
+            }
+
+
+
         }
     }
 
@@ -141,8 +262,18 @@ class Movie : AppCompatActivity() {
             companies.add(compani)
         }
         adapter = AdapterCompanies(companies)
-        findViewById<RecyclerView>(R.id.rv_companies).layoutManager = LinearLayoutManager(this)
-        findViewById<RecyclerView>(R.id.rv_companies).adapter = adapter
+        findViewById<RecyclerView>(R.id.rv_film).layoutManager = LinearLayoutManager(this.baseContext)
+        findViewById<RecyclerView>(R.id.rv_film).adapter = adapter
+    }
+
+    private fun initActors(Actors : List<Cast>){
+        for (act in Actors!!){
+            actores.add(act)
+        }
+        adapter2 = AdapterActor(actores)
+
+        findViewById<RecyclerView>(R.id.rv_film).layoutManager = LinearLayoutManager(this.baseContext)
+        findViewById<RecyclerView>(R.id.rv_film).adapter = adapter2
     }
 
 }
