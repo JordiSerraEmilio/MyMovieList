48998006c20b837b7b01e84b7c6c69a06d63fd69
JordiSerraEmilio IES Carles Vallbona <jordi_serraemilio@iescarlesvallbona.cat>
2022-05-26 15:08:01 +0200

    Commit and Push de ---> MultilanguageActivity Angel

---
+++ app/src/main/java/com/example/mymovielist/login/multilanguage/MultiLanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mymovielist.login.multilanguage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymovielist.R
+
+class MultiLanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_language)
+    }
+}
